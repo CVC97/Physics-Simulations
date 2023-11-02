27c0db501eaec6dbc42db3154e75c30dff36af33
Carlo von Carnap <carlo.voncarnap@stud.uni-goettingen.de>
2023-11-02 23:52:54 +0100

    Started spherical pendulum 2.

--- classical_mechanics/spherical_pendulum/spherical_pendulum_animation.py
+++ classical_mechanics/spherical_pendulum/spherical_pendulum_animation.py
@@ -1,6 +1,33 @@
 from manim import *
 
 
+# phase space class
+class PhaseSpace(Mobject):
+    def __init__(self, center, phase_array, side_length = 3, stroke_width = 1, labels = 0, **kwargs):
+        super().__init__(**kwargs)
+        self.center = center
+        self.side_length = side_length
+        self.stroke_with = stroke_width
+        self.x_array = phase_array[0]
+        self.xdot_array = phase_array[1]
+        self.x_inc = (max(self.x_array) - min(self.x_array)) / 10
+        self.xdot_inc = (max(self.xdot_array) - min(self.xdot_array)) / 10
+
+        square = Square(side_length = side_length, stroke_width = stroke_width, stroke_color = WHITE, **kwargs).move_to(center)
+        self.add(square)
+
+        if labels:
+            x_label = Tex(labels[0], color = WHITE, font_size = 156 / self.side_length).next_to(square, DOWN)
+            xdot_label = Tex(labels[1], color = WHITE, font_size = 156 / self.side_length).next_to(square, LEFT)
+            self.add(x_label, xdot_label)
+
+
+    def c2p(self, x, y, z):
+        ax = Axes(x_range = [min(self.x_array) - self.x_inc, max(self.x_array) + self.x_inc, 1], 
+                  y_range = [min(self.xdot_array) - self.xdot_inc, max(self.xdot_array) + self.xdot_inc, 1], 
+                  x_length = self.side_length, 
+                  y_length = self.side_length).move_to(self.center)
+        return ax.c2p(x, y, z)
 
 
 class spherical_pendulum_scene(ThreeDScene):
@@ -19,9 +46,9 @@ class spherical_pendulum_scene(ThreeDScene):
         CO3D_z_range = (0, 10.25, 1)
         ax = ThreeDAxes(
             x_range = CO3D_x_range, y_range = CO3D_y_range, z_range = CO3D_z_range,
-            x_length = 8, y_length = 8, z_length = 6, axis_config = {'tip_length': 0.05, 'tip_width': 0.3}, 
+            x_length = 8, y_length = 8, z_length = 5, axis_config = {'tip_length': 0.05, 'tip_width': 0.3}, 
             z_axis_config = {'color': WHITE},
-            ).set_opacity(0.1)
+            ).set_opacity(0.125)
     
         # spherical pendulum anchor
         prism_x = Prism(dimensions = [2, 0.125, 0.125], fill_color = WHITE, stroke_color = GREY).set_opacity(0.75).move_to(ax.c2p(0, 0, 10))
